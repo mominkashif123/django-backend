8a8eabb0f3fba5baa69cfb8b7bfbe4557000292f
Momin-star-beepboop <momin.kashif81@gmail.com>
2025-01-14 14:53:46 +0500

    cor

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3001', // Allow frontend's origin
+    origin: '*', // Allow frontend's origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
     credentials: true, // Allow cookies or authorization headers
   });
